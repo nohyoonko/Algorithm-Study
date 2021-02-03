ea164af04d3d110f51bd4ec9413ab5952697e840
Nohyoon Ko <kny_1011@naver.com>
2021-02-03 15:50:14 +0900

    [D3] 원재의 메모리 복구하기

---
+++ SWExpertAcademy/SWEA_Memory.java
@@ -0,0 +1,29 @@
+package com.ssafy.solve;
+
+import java.util.Scanner;
+
+public class SWEA_Memory {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			char[] m = sc.next().toCharArray();
+			int len = m.length;
+
+			int ans = 0;
+			char first = '0';
+			for (int i = 0; i < len; i++) {
+				// 앞뒤만 비교해서 다르면 바꿔주니까 pivot(first 변수)을 기준으로 비교만 하면 됨.
+				if (m[i] != first) {
+					ans++;
+					first = m[i];
+				}
+			}
+			System.out.println("#" + test_case + " " + ans);
+		}
+		
+		sc.close();
+	}
+}
