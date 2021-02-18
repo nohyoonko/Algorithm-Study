d90849e3ccafabe41cea69ca47deacec05414c55
Nohyoon Ko <kny_1011@naver.com>
2021-02-18 23:06:50 +0900

    #2669 직사각형 네개의 합집합의 면적 구하기 - 구현

---
+++ Baekjoon/Java/BOJ_2669.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 직사각형 네개의 합집합의 면적 구하기
+
+public class BOJ_2669 {
+	
+	static boolean[][] rectangle = new boolean[100][100];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int area = 0;
+		
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			for (int r = x1; r < x2; r++) {
+				for (int c = y1; c < y2; c++) {
+					if (!rectangle[r][c]) {
+						rectangle[r][c] = true;
+						area++;
+					}
+				}
+			}
+		}
+
+		System.out.println(area);
+	}
+
+}
