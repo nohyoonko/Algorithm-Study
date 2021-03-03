cc7d8f2f508507f041a94ff9d827798fcc29be57
Nohyoon Ko <kny_1011@naver.com>
2021-03-03 23:25:01 +0900

    H-Index - 정렬

---
+++ Programmers/Sort/H-Index.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        int len = citations.length;
+        
+        for (int i = 0; i < len; i++) {
+            // 인덱스로 나보다 큰 애들이 몇 편 있는지 확인 가능(h편 이상)
+            int h = len - i;
+            // 나의 인용 횟수가 h번 이상인지 확인
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
