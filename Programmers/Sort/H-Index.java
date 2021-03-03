0c8a516ae138d86a27365a9be63390e892aa963a
Nohyoon Ko <kny_1011@naver.com>
2021-03-03 23:26:52 +0900

    H-Index - 정렬 Level2

--- Programmers/Sort/H-Index.java
+++ Programmers/Sort/H-Index.java
@@ -4,9 +4,8 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         
-        Arrays.sort(citations);
+        Arrays.sort(citations); // 오름차순 정렬
         int len = citations.length;
-        
         for (int i = 0; i < len; i++) {
             // 인덱스로 나보다 큰 애들이 몇 편 있는지 확인 가능(h편 이상)
             int h = len - i;
